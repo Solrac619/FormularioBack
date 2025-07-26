3cd1786729a4b67b80dfbd0117bbb3c6cabe3462
Solrac619 <solracyestom2015@gmail.com>
2025-07-25 21:01:35 -0500

    .env y my sql

--- db/db.js
+++ db/db.js
@@ -1,18 +1,20 @@
+// db.js
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Carlos2001',
-  database: 'formulario_db',
-  port: 3307 
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 connection.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
   } else {
-    console.log('Conexión exitosa a MySQL');
+    console.log('✅ Conexión exitosa a MySQL (Railway)');
   }
 });
 
